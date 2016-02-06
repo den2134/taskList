1a5e4edf1337cb6c57949a0dfa70bdea706105e8
Denys Ponomarenko <qwe100com@mail.ru>
2016-02-06 13:56:13 +0200

    v.01

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Task list</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>You task list</h1>
+</div>
+
+<!-- Add jQuery, Bootstrap and .js -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
